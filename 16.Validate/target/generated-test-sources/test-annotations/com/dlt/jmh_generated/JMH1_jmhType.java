596ec184e3ae76feb5b06755c3a5200865c367ff
denglitong <litongdeng@qq.com>
2021-04-29 21:36:23 +0800

    update gitignore ignore target/ dir
    Signed-off-by: denglitong <litongdeng@qq.com>

--- 16.Validate/target/generated-test-sources/test-annotations/com/dlt/jmh_generated/JMH1_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.dlt.jmh_generated;
-public class JMH1_jmhType extends JMH1_jmhType_B3 {
-}
-
--- 16.Validate/target/generated-test-sources/test-annotations/com/dlt/jmh_generated/JMH2_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.dlt.jmh_generated;
-public class JMH2_jmhType extends JMH2_jmhType_B3 {
-}
-
